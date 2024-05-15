ec03229aefc04b4b325e06f1ae59181956385e76
d4kash <medakshjha@gmail.com>
2024-05-15 12:11:13 +0530

    initial commits, with code refactor

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutterlocalizationsample/application_localizations.dart';
+import 'home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      supportedLocales: [
+        Locale( 'en' , 'US' ),
+        Locale( 'es' , 'ES' ),
+        Locale( 'fr' , 'FR' ),
+      ],
+
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+locale: Locale( 'fr' , 'FR' ), // set locale for reflecting changes on app
+      localeResolutionCallback: (locale, supportedLocales) {
+        for (var supportedLocaleLanguage in supportedLocales) {
+          if (supportedLocaleLanguage.languageCode == locale?.languageCode &&
+              supportedLocaleLanguage.countryCode == locale?.countryCode) {
+            return supportedLocaleLanguage;
+          }
+        }
+        return supportedLocales.first;
+      },
+      home: Home(),
+    );
+  }
+}
